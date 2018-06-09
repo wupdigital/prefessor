111bbf7c5c3b2a620d8811a2b19be05135eace40
Balázs Varga <balazs.varga@wup.hu>
2018-06-09 22:16:56 +0200

    Implement prefessor for Js

--- prefessor-js/src/main/kotlin/digital/wup/prefessor/Prefessor.kt
+++ prefessor-js/src/main/kotlin/digital/wup/prefessor/Prefessor.kt
@@ -1,8 +1,14 @@
 package digital.wup.prefessor
 
 import kotlin.browser.localStorage
+import kotlin.collections.listOf
 import org.w3c.dom.Storage
+
 actual class Prefessor private constructor(private val storage: Storage = localStorage) {
+
+    private val editor by lazy {
+        PrefessorEditor(storage)
+    }
 
     actual companion object {
         actual fun create(): Prefessor {
@@ -18,7 +24,7 @@ actual class Prefessor private constructor(private val storage: Storage = localS
     }
 
     actual fun edit(): PrefessorEditor {
-        return PrefessorEditor()
+        return editor
     }
 
     /**
@@ -27,7 +33,7 @@ actual class Prefessor private constructor(private val storage: Storage = localS
      * @return Returns true if the preference exists in the preferences, otherwise false.
      */
     actual fun contains(key: String): Boolean {
-        TODO("not implemented")
+        return storage.getItem(key) != null
     }
 
     /**
@@ -37,7 +43,11 @@ actual class Prefessor private constructor(private val storage: Storage = localS
      * @return Returns the preference value if it exists, or defValue.
      */
     actual fun getBoolean(key: String, defValue: Boolean): Boolean {
-        TODO("not implemented")
+        return storage.getItem(key)?.let {
+            it as Boolean
+        } ?: run {
+            defValue
+        }
     }
 
     /**
@@ -47,7 +57,11 @@ actual class Prefessor private constructor(private val storage: Storage = localS
      * @return Returns the preference value if it exists, or defValue.
      */
     actual fun getFloat(key: String, defValue: Float): Float {
-        TODO("not implemented")
+        return storage.getItem(key)?.let {
+            it as Float
+        } ?: run {
+            defValue
+        }
     }
 
     /**
@@ -57,7 +71,11 @@ actual class Prefessor private constructor(private val storage: Storage = localS
      * @return Returns the preference value if it exists, or defValue.
      */
     actual fun getInt(key: String, defValue: Int): Int {
-        TODO("not implemented")
+        return storage.getItem(key)?.let {
+            it as Int
+        } ?: run {
+            defValue
+        }
     }
 
     /**
@@ -67,7 +85,11 @@ actual class Prefessor private constructor(private val storage: Storage = localS
      * @return Returns the preference value if it exists, or defValue.
      */
     actual fun getLong(key: String, defValue: Long): Long {
-        TODO("not implemented")
+        return storage.getItem(key)?.let {
+            it as Long
+        } ?: run {
+            defValue
+        }
     }
 
     /**
@@ -76,11 +98,13 @@ actual class Prefessor private constructor(private val storage: Storage = localS
      * @param key The name of the preference to retrieve.
      */
     actual fun getString(key: String, defValue: String): String {
-        TODO("not implemented")
+        return storage.getItem(key) ?: defValue
     }
 }
 
-actual class PrefessorEditor {
+actual class PrefessorEditor internal constructor(private val storage: Storage) {
+
+    private val pending = listOf<() -> Unit>()
 
     /**
      * Set a boolean value in the preferences editor, to be written back once {@link #apply()) are called.
@@ -88,7 +112,7 @@ actual class PrefessorEditor {
      * @param value The new value for the preference.
      */
     actual fun putBoolean(key: String, value: Boolean) {
-        TODO("not implemented")
+        storage.setItem(key, value.toString())
     }
 
     /**
@@ -97,7 +121,7 @@ actual class PrefessorEditor {
      * @param value The new value for the preference.
      */
     actual fun putFloat(key: String, value: Float) {
-        TODO("not implemented")
+        storage.setItem(key, value.toString())
     }
 
     /**
@@ -106,7 +130,7 @@ actual class PrefessorEditor {
      * @param value The new value for the preference.
      */
     actual fun putInt(key: String, value: Int) {
-        TODO("not implemented")
+        storage.setItem(key, value.toString())
     }
 
     /**
@@ -115,7 +139,7 @@ actual class PrefessorEditor {
      * @param value The new value for the preference.
      */
     actual fun putLong(key: String, value: Long) {
-        TODO("not implemented")
+        storage.setItem(key, value.toString())
     }
 
     /**
@@ -124,7 +148,7 @@ actual class PrefessorEditor {
      * @param value The new value for the preference.
      */
     actual fun putString(key: String, value: String) {
-        TODO("not implemented")
+        storage.setItem(key, value)
     }
 
     /**
@@ -133,7 +157,7 @@ actual class PrefessorEditor {
      * @param key The name of the preference to remove.
      */
     actual fun remove(key: String) {
-        TODO("not implemented")
+        storage.removeItem(key)
     }
 
     /**
@@ -143,10 +167,9 @@ actual class PrefessorEditor {
      * regardless of whether you called clear before or after put methods on this editor.
      */
     actual fun clear() {
-        TODO("not implemented")
+        storage.clear()
     }
 
     actual fun apply() {
-        TODO("not implemented")
     }
 }
