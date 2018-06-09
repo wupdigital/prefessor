c736708cbf90f93a345edfa1eab6565adb78cae7
Balázs Varga <balazs.varga@wup.hu>
2018-06-09 11:48:02 +0200

    Restrict ContextProvider class to library scope and sealed Jvm static get metod for ContextProvider

--- prefessor-android/src/main/kotlin/digital/wup/prefessor/ContextProvider.kt
+++ prefessor-android/src/main/kotlin/digital/wup/prefessor/ContextProvider.kt
@@ -1,17 +1,19 @@
 package digital.wup.prefessor
 
 import android.content.Context
+import android.support.annotation.RestrictTo
 
-public class ContextProvider {
+@RestrictTo(RestrictTo.Scope.LIBRARY)
+sealed class ContextProvider {
     companion object {
+        @JvmStatic
         fun get(): Context {
-
             synchronized(this) {
-                if (ContextContentProvider.ctx == null) {
-                    throw Exception("Contex is null")
+                ContextContentProvider.ctx?.let {
+                    return it
+                } ?: run {
+                    throw IllegalStateException("Context == null")
                 }
-
-                return ContextContentProvider.ctx!!
             }
         }
     }
