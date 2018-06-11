f875dd8bf7968fc0d3e1b426a5306ed68c1c3405
Balázs Varga <balazs.varga@wup.hu>
2018-06-11 09:27:33 +0200

    Implement prefessor on iOS

--- prefessor-ios/src/main/kotlin/digital/wup/prefessor/Prefessor.kt
+++ prefessor-ios/src/main/kotlin/digital/wup/prefessor/Prefessor.kt
@@ -1,9 +1,21 @@
 package digital.wup.prefessor
 
-actual class Prefessor {
+/* ktlint-disable no-wildcard-imports */
+import kotlinx.cinterop.*
+import platform.Foundation.*
+import platform.objc.*
+/* ktlint-enable no-wildcard-imports */
+import kotlin.collections.mutableListOf
+
+actual class Prefessor private constructor(private val userDefaults: NSUserDefaults) {
+
+    private val editor by lazy {
+        PrefessorEditor(userDefaults)
+    }
+
     actual companion object {
         actual fun create(): Prefessor {
-            return Prefessor()
+            return Prefessor(NSUserDefaults.standardUserDefaults)
         }
     }
 
@@ -13,7 +25,7 @@ actual class Prefessor {
      * @return Returns true if the preference exists in the preferences, otherwise false.
      */
     actual fun contains(key: String): Boolean {
-        return false
+        return userDefaults.objectForKey(key) != null
     }
 
     /**
@@ -23,7 +35,7 @@ actual class Prefessor {
      * @return Returns the preference value if it exists, or defValue.
      */
     actual fun getBoolean(key: String, defValue: Boolean): Boolean {
-        return false
+        return userDefaults.objectForKey(key) as Boolean? ?: defValue
     }
 
     /**
@@ -33,7 +45,7 @@ actual class Prefessor {
      * @return Returns the preference value if it exists, or defValue.
      */
     actual fun getFloat(key: String, defValue: Float): Float {
-        return 0.0f
+        return userDefaults.objectForKey(key) as Float? ?: defValue
     }
 
     /**
@@ -43,7 +55,7 @@ actual class Prefessor {
      * @return Returns the preference value if it exists, or defValue.
      */
     actual fun getInt(key: String, defValue: Int): Int {
-        return 0
+        return userDefaults.objectForKey(key) as Int? ?: defValue
     }
 
     /**
@@ -53,7 +65,7 @@ actual class Prefessor {
      * @return Returns the preference value if it exists, or defValue.
      */
     actual fun getLong(key: String, defValue: Long): Long {
-        return 0L
+        return userDefaults.objectForKey(key) as Long? ?: defValue
     }
 
     /**
@@ -62,61 +74,101 @@ actual class Prefessor {
      * @param key The name of the preference to retrieve.
      */
     actual fun getString(key: String, defValue: String): String {
-        return ""
+        return userDefaults.objectForKey(key) as String? ?: defValue
     }
 
     actual fun edit(): PrefessorEditor {
-        return PrefessorEditor()
+        return editor
     }
 }
 
-actual class PrefessorEditor {
+actual class PrefessorEditor internal constructor(private val userDefaults: NSUserDefaults) {
+
+    private val pending = mutableListOf<() -> Unit>()
+
     /**
      * Set a boolean value in the preferences editor, to be written back once {@link #apply()) are called.
      * @param key The name of the preference to modify.
      * @param value The new value for the preference.
      */
-    actual fun putBoolean(key: String, value: Boolean) { }
+    actual fun putBoolean(key: String, value: Boolean) {
+        pending.add {
+            userDefaults.setBool(value, key)
+        }
+    }
 
     /**
      * Set a float value in the preferences editor, to be written back once {@link #apply()) are called.
      * @param key The name of the preference to modify.
      * @param value The new value for the preference.
      */
-    actual fun putFloat(key: String, value: Float) { }
+    actual fun putFloat(key: String, value: Float) {
+        pending.add {
+            userDefaults.setFloat(value, key)
+        }
+    }
 
     /**
      * Set an int value in the preferences editor, to be written back once {@link #apply()) are called.
      * @param key The name of the preference to modify.
      * @param value The new value for the preference.
      */
-    actual fun putInt(key: String, value: Int) { }
+    actual fun putInt(key: String, value: Int) {
+        pending.add {
+            userDefaults.setObject(value, key)
+        }
+    }
 
     /**
      * Set a long value in the preferences editor, to be written back once {@link #apply()) are called.
      * @param key The name of the preference to modify.
      * @param value The new value for the preference.
      */
-    actual fun putLong(key: String, value: Long) { }
+    actual fun putLong(key: String, value: Long) {
+        pending.add {
+            userDefaults.setInteger(value, key)
+        }
+    }
 
     /**
      * Set a string value in the preferences editor, to be written back once {@link #apply()) are called.
      * @param key The name of the preference to modify.
      * @param value The new value for the preference.
      */
-    actual fun putString(key: String, value: String) { }
+    actual fun putString(key: String, value: String) {
+        pending.add {
+            userDefaults.setObject(value, key)
+        }
+    }
 
     /**
      * Mark in the editor that a preference value should be removed, which will be done in the actual preferences once {@link #commit()} is called.
      * @param key The name of the preference to remove.
      */
-    actual fun remove(key: String) { }
+    actual fun remove(key: String) {
+        pending.add {
+            userDefaults.removeObjectForKey(key)
+        }
+    }
 
     /**
      * Mark in the editor to remove all values from the preferences. Once commit is called, the only remaining preferences will be any that you have defined in this editor.
      * Note that when committing back to the preferences, the clear is done first, regardless of whether you called clear before or after put methods on this editor.
      */
-    actual fun clear() { }
+    actual fun clear() {
+        pending.add {
+            val dict = userDefaults.dictionaryRepresentation()
+            dict.keys.forEach {
+                userDefaults.removeObjectForKey(it as String)
+            }
+        }
+    }
 
-    actual fun apply() { }
+    actual fun apply() {
+        pending.forEach {
+            it()
+        }
+        // clear pendings after apply
+        pending.removeAll { true }
+    }
 }
